ad1c8d05f6abb76bbb70b12787c40c9b17075756
Jose Martinez Fuentes <jose.martinez@tokiota.com>
2018-03-09 10:43:04 +0100

    Add navbar options depends on the user role

--- sound-catalog/src/app/nav-bar/nav-bar.component.ts
+++ sound-catalog/src/app/nav-bar/nav-bar.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../shared/services/auth.service';
 // Redux
 import { NgRedux, select } from '@angular-redux/store';
 import { IAppState } from '../state/store';
+import { AuthorizedUser } from '../shared/models/authorizedUser';
 
 @Component({
   selector: 'app-nav-bar',
@@ -19,6 +20,10 @@ export class NavBarComponent implements OnInit {
   private lastname: string;
   private username: string;
   private role: string;
+  private isAuthenticated: boolean;
+  private isGuest: boolean;
+  private isAdmin: boolean;
+  private isContributor: boolean;
 
   constructor(
     private ngRedux: NgRedux<IAppState>,
@@ -32,12 +37,32 @@ export class NavBarComponent implements OnInit {
       })
     });
 
-    if (this.authService.isAuthenticated()) {
-      const user = this.ngRedux.getState().user;
+    this.isAuthenticated = false;
+
+    this.ngRedux.subscribe(() => {
+      this.updateNavbar(this.ngRedux.getState().user);
+    });
+   }
+
+   updateNavbar(user: AuthorizedUser) {
+    if (user == null) {
+      this.isAuthenticated = false;
+    } else {
       this.firstname = user.firstname;
       this.lastname = user.lastname;
       this.username = user.username;
       this.role = user.role;
+      this.isAuthenticated = true;
+      switch (this.role) {
+        case 'Admin':
+           this.isAdmin = true;
+           break;
+        case 'Contributor':
+           this.isContributor = true;
+           break;
+        default:
+          this.isGuest = true;
+      }
     }
    }
 }
